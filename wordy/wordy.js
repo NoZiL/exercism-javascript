2f1c67bd52f52a021474f84338c5b680b5c5d74b
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2020-10-15 06:48:17 +0000

    Wordy it

--- wordy/wordy.js
+++ wordy/wordy.js
@@ -1,7 +1,23 @@
+const regex = /What is (?<number>-?\d*)(\s(?<operation>.*)\s(?<number2>-?\d*))?\?/;
+
 export const answer = (question = "") => {
-  const { groups } = question.match(/What is (?<number>\d*)\?/);
-  if (groups.number) {
-    return Number(groups.number);
+  const { groups } = question.match(regex);
+  const { number, operation, number2 } = groups;
+  switch (true) {
+    default:
+    case !number:
+      return null;
+    case !operation:
+      return Number(number);
+    case operation === "plus":
+      return Number(number) + Number(number2);
+    case operation === "minus":
+      return Number(number) - Number(number2);
+    case operation === "multiplied by":
+      return Number(number) * Number(number2);
+    case operation === "divided by":
+      return Number(number) / Number(number2);
+    case !!operation:
+      throw new Error("Unknown operation");
   }
-  throw new Error("Not yet implemented");
 };
