de60f7661b8063f0496a7c2837079c6e0dcf42fd
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2020-10-19 13:24:31 +0000

    Wordy solution

--- wordy/wordy.js
+++ wordy/wordy.js
@@ -1,38 +1,33 @@
-const regex = /What is(?<operations>.*)\?/;
-
 const OPERATIONS = {
+  "What is": (_, b) => b,
   plus: (a, b) => a + b,
   minus: (a, b) => a - b,
   "multiplied by": (a, b) => a * b,
   "divided by": (a, b) => a / b,
 };
 
+const questionRegex = /^(?<question>.*)\?$/;
+
 export function answer(question = "") {
-  const match = question.match(regex);
-  if (!match) {
-    throw new Error("Unknown operation");
-  }
-  if (!match.groups.operations) {
-    throw new Error("Syntax error");
-  }
-  const operations = match.groups.operations.trim().split(/\s(?!by)/);
-  return calculate(operations);
+  return calculate(
+    question
+      .match(questionRegex)
+      .groups.question.trim()
+      .split(/\s(?!by|is)/)
+  );
 }
 
-function calculate(operations = [], acc = null) {
-  const [head, ...tail] = operations;
-  if (operations.length <= 0) return acc;
-  const numberValue = Number.parseInt(head);
+function calculate([head, ...tail], acc = 0) {
   switch (true) {
-    case !isNaN(numberValue) && acc === null:
-      return calculate(tail, numberValue);
-    case !isNaN(numberValue) && acc !== null:
-      throw new Error("Syntax error");
+    case !head:
+      return acc;
     case Object.keys(OPERATIONS).includes(head):
       return calculate(
         tail.slice(1),
         calculateOne(OPERATIONS[head], acc, tail[0])
       );
+    case !isNaN(Number.parseInt(head)):
+      throw new Error("Syntax error");
     default:
       throw new Error("Unknown operation");
   }
