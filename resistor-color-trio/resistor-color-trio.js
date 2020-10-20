8f57f4c148574dbabf4043c6422ea00435a31751
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2020-10-20 07:12:06 +0000

    Resistor color trio solution

--- resistor-color-trio/resistor-color-trio.js
+++ resistor-color-trio/resistor-color-trio.js
@@ -1,14 +1,36 @@
-//
-// This is only a SKELETON file for the 'Resistor Color Trio' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
 
 export class ResistorColorTrio {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(colors = []) {
+    const [digit1, digit2, digit3] = colors.map((e) => {
+      const value = COLORS.findIndex((c) => c === e);
+      if (value === -1) throw new Error("invalid color");
+      return value;
+    });
+    this.value = (digit1 * 10 + digit2) * Math.pow(10, digit3);
   }
 
-  label() {
-    throw new Error("Remove this statement and implement this function");
+  get label() {
+    return `Resistor value: ${labelUnit(this.value, "ohms")}`;
+  }
+}
+
+function labelUnit(value = 0, unit = "") {
+  switch (true) {
+    case value >= 1_000:
+      return `${value / 1_000} kilo${unit}`;
+    default:
+      return `${value} ${unit}`;
   }
 }
