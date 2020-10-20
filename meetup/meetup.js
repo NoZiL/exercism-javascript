16b6c3aea4c364c61f9b7e95e35e9b6612e5726a
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2020-10-20 10:27:52 +0000

    Fix Meetup

--- meetup/meetup.js
+++ meetup/meetup.js
@@ -1,44 +1,50 @@
-const days = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday'
-]
+const DAYS = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-export const meetupDay = (year, month, day, schedule) => {
-  const first_day = find_first_date(schedule, year, month)
-  const step = schedule == 'last' ? -1 : 1
-  return find_day(first_day, day, step, new Date(year, month + 1), -1)
+export function meetup(year = 0, month = 1, schedule = "", day = "") {
+  const first_day = getFirstDay(year, month - 1, schedule);
+  const step = schedule === "last" ? -1 : 1;
+  return findDayInMonth(first_day, day, step, new Date(year, month + 1), -1);
 }
 
-const find_first_date = (schedule, year, month) => {
+function getFirstDay(year = 0, month = 0, schedule = "") {
   switch (schedule) {
-    case '1st':
-      return new Date(year, month)
-    case '2nd':
-      return new Date(year, month, 1 + 7)
-    case '3rd':
-      return new Date(year, month, 1 + 7 * 2)
-    case '4th':
-      return new Date(year, month, 1 + 7 * 3)
-    case '5th':
-      return new Date(year, month, 1 + 7 * 4)
-    case 'teenth':
-      return new Date(year, month, 13)
-    case 'last':
-      return add_days(new Date(year, month + 1), -1)
+    case "first":
+      return new Date(year, month, 1 + 7 * 0);
+    case "second":
+      return new Date(year, month, 1 + 7 * 1);
+    case "third":
+      return new Date(year, month, 1 + 7 * 2);
+    case "fourth":
+      return new Date(year, month, 1 + 7 * 3);
+    case "fifth":
+      return new Date(year, month, 1 + 7 * 4);
+    case "teenth":
+      return new Date(year, month, 13);
+    case "last":
+      return addDays(new Date(year, month + 1), -1);
     default:
-      throw new Error("invalid schedule")
+      throw new Error("invalid schedule");
   }
 }
 
-const find_day = (date, day, step, max_date) => {
-  if(step == 1 && date > max_date) throw new Error("not found")
-  if(date.getDay() == days.findIndex(e => e == day)) return date
-  return find_day(add_days(date, step), day, step, max_date)
+function findDayInMonth(
+  date = new Date(),
+  day = 0,
+  step = 1,
+  firstDateNextMonth = new Date()
+) {
+  if (step === 1 && date > firstDateNextMonth) throw new Error("not found");
+  if (date.getDay() === DAYS.findIndex((e) => e === day)) return date;
+  return findDayInMonth(addDays(date, step), day, step, firstDateNextMonth);
 }
 
-const add_days = (date, days) => new Date((new Date(date)).setDate(date.getDate() + days))
+const addDays = (date = new Date(), days = 1) =>
+  new Date(new Date(date).setDate(date.getDate() + days));
