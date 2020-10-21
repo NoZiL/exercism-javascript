5e5301576df5158e041e28edc0135047d902f65b
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2020-10-21 08:57:52 +0000

    Update change

--- change/change.js
+++ change/change.js
@@ -1,8 +1,8 @@
 export class Change {
   calculate(available_cuts, change_needed) {
-    if(change_needed == 0) return []
-    const fiting_cuts = available_cuts.filter(e => e <= change_needed)
-    const cut = fiting_cuts.reverse()[0]
-    return [...this.calculate(available_cuts, change_needed - cut), cut]
+    if (change_needed == 0) return [];
+    const fiting_cuts = available_cuts.filter((e) => e <= change_needed);
+    const cut = fiting_cuts.reverse()[0];
+    return [...this.calculate(available_cuts, change_needed - cut), cut];
   }
 }
