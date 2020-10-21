9ca42b72437e2d36b90022652ef0f0a9e2c547f1
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2020-10-21 06:17:11 +0000

    Update linked list

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -1,63 +1,67 @@
 export class LinkedList {
   constructor() {
-    this.head = this.tail = undefined
+    this.head = this.tail = undefined;
   }
 
   _insertAfter(value, previous) {
-    const next = previous ? previous.next : this.head
-    const node = { value, previous, next }
+    const next = previous ? previous.next : this.head;
+    const node = { value, previous, next };
 
-    if(previous) previous.next = node
-    else this.head = node
+    if (previous) previous.next = node;
+    else this.head = node;
 
-    if(next) next.previous = node
-    else this.tail = node
+    if (next) next.previous = node;
+    else this.tail = node;
   }
 
   _drop(node) {
-    const previous = node.previous
-    const next = node.next
+    const previous = node.previous;
+    const next = node.next;
 
-    if(previous) previous.next = next
-    else this.head = next
+    if (previous) previous.next = next;
+    else this.head = next;
 
-    if(next) next.previous = previous
-    else this.tail = previous
+    if (next) next.previous = previous;
+    else this.tail = previous;
 
-    return node.value
+    return node.value;
   }
 
   push(value) {
-    this._insertAfter(value, this.tail)
+    this._insertAfter(value, this.tail);
   }
 
   pop() {
-    return this._drop(this.tail)
+    return this._drop(this.tail);
   }
 
   shift() {
-    return this._drop(this.head)
+    return this._drop(this.head);
   }
 
   unshift(value) {
-    this._insertAfter(value)
+    this._insertAfter(value);
   }
 
   count() {
-    const traverse = 'next'
+    const traverse = "next";
     const recursiveCount = (node) =>
-      node ? recursiveCount(node[traverse]) + 1 : 0
-    return recursiveCount(this.head)
+      node ? recursiveCount(node[traverse]) + 1 : 0;
+    return recursiveCount(this.head);
   }
 
   delete(value) {
-    const traverse = 'next'
+    const traverse = "next";
     const recursiveDelete = (node) => {
-      if(node) {
-        if(node.value === value) this._drop(node)
-        recursiveDelete(node[traverse])
+      switch (true) {
+        case node?.value === value:
+          this._drop(node);
+          break;
+        case !!node:
+          recursiveDelete(node[traverse]);
+        default:
       }
-    }
-    recursiveDelete(this.head, 'next', value)
+    };
+    recursiveDelete(this.head, "next", value);
   }
 }
