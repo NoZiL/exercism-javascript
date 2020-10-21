dcaa824e897c0d6abd0d8127c2a84f05460fef55
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2020-10-21 06:06:15 +0000

    Update bob

--- bob/bob.js
+++ bob/bob.js
@@ -1,10 +1,17 @@
 export const hey = (messageNonTrimmed) => {
-  const message = messageNonTrimmed.trim()
-  const shouting = !message.match(/[a-z]/) && message.match(/[A-Z]/)
-  const question = message.match(/\?+$/)
-  if (!message) return 'Fine. Be that way!'
-  if (shouting && question) return 'Calm down, I know what I\'m doing!'
-  if (shouting) return 'Whoa, chill out!'
-  if (question) return 'Sure.'
-  return 'Whatever.'
-}
+  const message = messageNonTrimmed.trim();
+  const shouting = !message.match(/[a-z]/) && !!message.match(/[A-Z]/);
+  const question = !!message.match(/\?+$/);
+  switch (true) {
+    case !message:
+      return "Fine. Be that way!";
+    case shouting && question:
+      return "Calm down, I know what I'm doing!";
+    case shouting:
+      return "Whoa, chill out!";
+    case question:
+      return "Sure.";
+    default:
+      return "Whatever.";
+  }
+};
