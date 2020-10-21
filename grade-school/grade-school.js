5fb31ed99a21637c486ff83828956f33b5ee927c
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2020-10-21 06:10:51 +0000

    Update grade school

--- grade-school/grade-school.js
+++ grade-school/grade-school.js
@@ -1,23 +1,23 @@
 export class GradeSchool {
   constructor() {
-    this.data = {}
+    this.data = {};
   }
   roster() {
     return Object.entries(this.data).reduce(
       (accumulator, [grade, names]) => ({
         ...accumulator,
-        [grade]: [...names]
+        [grade]: [...names],
       }),
       {}
-    )
+    );
   }
   add(name, grade) {
     this.data = {
       ...this.data,
-      [grade]: ((this.data[grade] || []).concat(name)).sort()
-    }
+      [grade]: (this.data[grade] || []).concat(name).sort(),
+    };
   }
   grade(grade) {
-    return [...(this.data[grade] || [])]
+    return [...(this.data[grade] || [])];
   }
 }
