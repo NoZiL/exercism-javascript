a2d8db70f2448b8496fc0a15458f53608373574e
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2020-10-21 06:30:46 +0000

    Update luhn

--- luhn/luhn.js
+++ luhn/luhn.js
@@ -1,21 +1,20 @@
-export class Luhn {
-  constructor(string) {
-    this.numbers =
-      string
-        .split("")
-        .filter(e => e != ' ')
-        .map(e => parseInt(e))
-  }
-
-  get valid() {
-    if(this.numbers.length <= 1) return false
-    if(this.numbers.some(e => isNaN(e))) return false
-    const sum =
-      [...this.numbers]
-        .reverse().map((e, index) => index % 2 == 1 ? double(e) : e)
-        .reduce((acc, e) => acc + e, 0)
-    return sum % 10 == 0
-  }
+export function valid(string = "") {
+  const numbers = getNumbers(string);
+  if (numbers.length <= 1) return false;
+  if (numbers.some((e) => isNaN(e))) return false;
+  const sum = numbers.reduceRight(
+    (acc, number, index) =>
+      acc + ((numbers.length - 1 - index) % 2 == 1 ? double(number) : number),
+    0
+  );
+  return sum % 10 === 0;
 }
 
-const double = e => e * 2 >= 10 ? e * 2 - 9 : e * 2
+function getNumbers(string = []) {
+  return string
+    .split("")
+    .filter((e) => e != " ")
+    .map((e) => parseInt(e));
+}
+
+const double = (e) => (e * 2 >= 10 ? e * 2 - 9 : e * 2);
